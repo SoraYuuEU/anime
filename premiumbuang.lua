f23c7b65ce9e10137c462bf023ea99e6ce2cfd85
SoraYuuEU <azdiva3531@gmail.com>
2024-07-26 10:41:52 +0700

    Create premiumbuang.lua

---
+++ premiumbuang.lua
@@ -0,0 +1,20 @@
+if not game:IsLoaded() then
+	game.Loaded:Wait()
+end
+
+task.wait( math.random() )
+
+local games = {
+	[34121350] = 'https://api.luarmor.net/files/v3/loaders/b648cec51b1d83aab602bbee4e2a8c6f.lua' -- AD
+}
+
+if games[game.CreatorId] then
+	if game.CreatorId == 34121350 then
+		repeat
+			loadstring(game:HttpGet(games[game.CreatorId]))()
+			task.wait(10)
+		until getgenv().buanghub ~= nil;
+	else
+		loadstring(game:HttpGet(games[game.CreatorId]))()
+	end
+end
